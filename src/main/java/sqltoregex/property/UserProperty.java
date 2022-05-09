35a28c8c45b9ef12444d8323d7fd2c8e93db70f6
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-09 18:06:49 +0200

    changed UserProperty to use Map with Property Interface

--- src/main/java/sqltoregex/property/UserProperty.java
+++ src/main/java/sqltoregex/property/UserProperty.java
@@ -1,16 +1,18 @@
 package sqltoregex.property;
 
+import com.google.common.collect.ImmutableMap;
+
 import java.util.Map;
 
 class UserProperty{
     private static UserProperty instance;
-    public Map<Class<Object>, Object> propertyMap;
+    public ImmutableMap<Class<Property>, Property> propertyMap;
 
-    private UserProperty(Map<Class<Object>, Object> map){
-        this.propertyMap = map;
+    private UserProperty(Map<Class<Property>, Property> map){
+        this.propertyMap = new ImmutableMap.Builder<Class<Property>, Property>().putAll(map).build();
     }
 
-    public static UserProperty getInstance(Map<Class<Object>, Object> map) {
+    public static UserProperty getInstance(Map<Class<Property>, Property> map) {
         if (instance == null){
             instance = new UserProperty(map);
         }
