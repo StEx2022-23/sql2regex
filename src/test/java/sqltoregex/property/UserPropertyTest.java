5049f87032f936e752e05aadc1ebbfa8e6961fa2
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-09 17:38:39 +0200

    added UserPropertyTest

---
+++ src/test/java/sqltoregex/property/UserPropertyTest.java
@@ -0,0 +1,12 @@
+package sqltoregex.property;
+
+import org.junit.jupiter.api.Test;
+
+public class UserPropertyTest {
+
+    @Test
+    public void SingletonTest(){
+        Map<Class<Object>, Object>
+        UserProperty userProperty = new UserProperty();
+    }
+}
