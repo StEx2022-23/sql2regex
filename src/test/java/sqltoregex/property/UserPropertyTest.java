b2deaaf697b39a562fb8e35e0ec209126ae5b650
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-09 18:06:58 +0200

    added UserPropertyTest

--- src/test/java/sqltoregex/property/UserPropertyTest.java
+++ src/test/java/sqltoregex/property/UserPropertyTest.java
@@ -1,12 +1,26 @@
 package sqltoregex.property;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class UserPropertyTest {
 
     @Test
-    public void SingletonTest(){
-        Map<Class<Object>, Object>
-        UserProperty userProperty = new UserProperty();
+    public void onlyOneInstance(){
+        Map<Class<Property>, Property> map = new HashMap<>();
+        UserProperty userProperty1 =  UserProperty.getInstance(map);
+        UserProperty userProperty2 = UserProperty.getInstance(map);
+        Assertions.assertEquals(userProperty1, userProperty2);
+    }
+
+    @Test
+    public void MapNotMutable(){
+        Map<Class<Property>, Property> map = new HashMap<>();
+        UserProperty userProperty1 =  UserProperty.getInstance(map);
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> userProperty1.propertyMap.putAll(map));
+        Assertions.assertThrows(UnsupportedOperationException.class, () -> userProperty1.propertyMap.put(OrderRotation.class);
     }
 }
