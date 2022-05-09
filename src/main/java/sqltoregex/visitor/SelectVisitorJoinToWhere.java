92a0ab4a4918d8d4482568125ed24b9636d080c0
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-09 17:38:31 +0200

    added SelectVisitorJointoWhere

---
+++ src/main/java/sqltoregex/visitor/SelectVisitorJoinToWhere.java
@@ -0,0 +1,39 @@
+package sqltoregex.visitor;
+
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.statement.select.Join;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.SelectVisitorAdapter;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Visitor for transforming JOIN-ON-expressions into WHERE-expressions. Deletes the JOIN-ON-expressions after
+ * transformation.
+ */
+public class SelectVisitorJoinToWhere extends SelectVisitorAdapter {
+    @Override
+    public void visit(PlainSelect plainSelect) {
+        List<Join> joinList = plainSelect.getJoins();
+
+        if (joinList != null) {
+            for (Join join : joinList) {
+                //In MySQL, it is not possible to have multiple ON, all are concatenated using "AND" that's why the
+                // OnExpressionList has always 1 Element for our useCase
+                if (join.getOnExpressions().stream().findFirst().isPresent()) {
+                    if (plainSelect.getWhere() == null) {
+                        plainSelect.setWhere(join.getOnExpressions().stream().findFirst().get());
+                    } else {
+                        plainSelect.setWhere(new AndExpression().withLeftExpression(plainSelect.getWhere())
+                                                     .withRightExpression(join.getOnExpressions().stream()
+                                                                                  .findFirst().get()));
+                    }
+                }
+            }
+            for (Join join : joinList) {
+                join.setOnExpressions(new LinkedList<>());
+            }
+        }
+    }
+}
