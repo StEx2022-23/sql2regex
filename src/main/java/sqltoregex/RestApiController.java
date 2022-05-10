52499062996174850e5ab0f674beffbb3247a645
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-10 17:23:11 +0200

    commented RestAPIController for compatability reasons

--- src/main/java/sqltoregex/RestApiController.java
+++ src/main/java/sqltoregex/RestApiController.java
@@ -8,30 +8,30 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class RestApiController {
-
-    private final SqlRegex sqlregex = new SqlRegex();
-
-    public SqlRegex getSqlRegex() {
-        return sqlregex;
-    }
-
-    @GetMapping("/convert")
-    public String returnGivenParam(@RequestParam(value = "sql", defaultValue = "Pass your SQL-Statement!") String sql) {
-        this.getSqlRegex().setSql(sql);
-        this.getSqlRegex().convert();
-        return this.getSqlRegex().toString();
-    }
-
-    @PostMapping("/convert")
-    public String convertSql2Regex(@RequestBody SqlRegex sqlregex) {
-        sqlregex.convert();
-        return sqlregex.toString();
-    }
-
-    @PostMapping("/multiconvert")
-    public String convertSql2RegexMulti(@RequestBody SqlRegex[] sqlregexlist) {
-        MultiSqlRegex sqlregexmultargs = new MultiSqlRegex(List.of(sqlregexlist));
-        sqlregexmultargs.convert();
-        return sqlregexmultargs.toString();
-    }
+//
+//    private final SqlRegex sqlregex = new SqlRegex();
+//
+//    public SqlRegex getSqlRegex() {
+//        return sqlregex;
+//    }
+//
+//    @GetMapping("/convert")
+//    public String returnGivenParam(@RequestParam(value = "sql", defaultValue = "Pass your SQL-Statement!") String sql) {
+//        this.getSqlRegex().setSql(sql);
+//        this.getSqlRegex().convert();
+//        return this.getSqlRegex().toString();
+//    }
+//
+//    @PostMapping("/convert")
+//    public String convertSql2Regex(@RequestBody SqlRegex sqlregex) {
+//        sqlregex.convert();
+//        return sqlregex.toString();
+//    }
+//
+//    @PostMapping("/multiconvert")
+//    public String convertSql2RegexMulti(@RequestBody SqlRegex[] sqlregexlist) {
+//        MultiSqlRegex sqlregexmultargs = new MultiSqlRegex(List.of(sqlregexlist));
+//        sqlregexmultargs.convert();
+//        return sqlregexmultargs.toString();
+//    }
 }
