6df1dccb78a514a02fc7fe4cd45a40e9e8cc400d
binkertpat <58105072+binkertpat@users.noreply.github.com>
2022-05-14 13:02:14 +0200

    removed no needed public keyword

--- src/main/java/sqltoregex/property/regexgenerator/RegExGenerator.java
+++ src/main/java/sqltoregex/property/regexgenerator/RegExGenerator.java
@@ -1,6 +1,6 @@
 package sqltoregex.property.regexgenerator;
 
 public interface RegExGenerator<T> {
-    public String generateRegExFor(T wordToFindSynonyms);
-    public void setCapturingGroup(boolean capturingGroup);
+    String generateRegExFor(T wordToFindSynonyms);
+    void setCapturingGroup(boolean capturingGroup);
 }
