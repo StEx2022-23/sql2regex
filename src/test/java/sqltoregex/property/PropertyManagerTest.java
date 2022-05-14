8465fe4d86ccfccc14034bbbf453dfc9f8209659
binkertpat <58105072+binkertpat@users.noreply.github.com>
2022-05-14 09:52:25 +0200

    refactor property manager test

--- src/test/java/sqltoregex/property/PropertyManagerTest.java
+++ src/test/java/sqltoregex/property/PropertyManagerTest.java
@@ -1,23 +1,34 @@
 package sqltoregex.property;
 
-import org.apache.commons.lang3.tuple.MutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.junit.jupiter.api.*;
 import org.xml.sax.SAXException;
-import sqltoregex.property.PropertyManager;
-import sqltoregex.property.PropertyOption;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 class PropertyManagerTest {
     PropertyManager propertyManager = new PropertyManager();
+    static Set<PropertyOption> spellings;
+    static Set<PropertyOption> orders;
+    static Set<SimpleDateFormat> dateFormats;
+    static Set<SimpleDateFormat> timeFormats;
+    static Set<SimpleDateFormat> dateTimeFormats;
+    static Set<String> aggregateFunctionLang;
+    static String sql;
+
+    @BeforeAll
+    static void resetSets(){
+        spellings = new HashSet<>();
+        orders = new HashSet<>();
+        dateFormats = new HashSet<>();
+        timeFormats = new HashSet<>();
+        dateTimeFormats = new HashSet<>();
+        aggregateFunctionLang = new HashSet<>();
+        sql = "SELECT * FROM test";
+    }
 
     PropertyManagerTest() throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
     }
@@ -46,17 +57,10 @@ class PropertyManagerTest {
 
     @Test
     void testUserConfigurations() {
-        Set<PropertyOption> spellings = new HashSet<>();
         spellings.add(PropertyOption.KEYWORDSPELLING);
-        Set<PropertyOption> orders = new HashSet<>();
         orders.add(PropertyOption.COLUMNNAMEORDER);
-        Set<SimpleDateFormat> dateFormats = new HashSet<>();
         dateFormats.add(new SimpleDateFormat("yyyy-MM-dd"));
-        Set<SimpleDateFormat> timeFormats = new HashSet<>();
-        Set<SimpleDateFormat> dateTimeFormats = new HashSet<>();
-        Set<String> aggregateFunctionLang = new HashSet<>();
         aggregateFunctionLang.add("SUM,SUMME");
-        String sql = "Select * From test";
         PropertyForm propertyForm = new PropertyForm(
                 spellings, orders, dateFormats, timeFormats, dateTimeFormats, aggregateFunctionLang, sql
         );
@@ -74,16 +78,26 @@ class PropertyManagerTest {
     }
 
     @Test
-    void testUserConfigurationsEverythingDisabeld() {
-        Set<PropertyOption> spellings = new HashSet<>();
-        Set<PropertyOption> orders = new HashSet<>();
-        Set<SimpleDateFormat> dateFormats = new HashSet<>();
-        Set<SimpleDateFormat> timeFormats = new HashSet<>();
-        Set<SimpleDateFormat> dateTimeFormats = new HashSet<>();
-        Set<String> aggregateFunctionLang = new HashSet<>();
-        String sql = "Select * From test";
+    void testUserConfigurationsWhenEverythingIsDisabeld() {
         PropertyForm propertyForm = new PropertyForm(spellings, orders, dateFormats, timeFormats, dateTimeFormats, aggregateFunctionLang, sql);
         PropertyMapBuilder propertyMapBuilder = new PropertyMapBuilder();
         Assertions.assertEquals(propertyMapBuilder.build(), propertyManager.parseUserOptionsInput(propertyForm));
+    }
+
+    @Test
+    void testAggregateFunctionLangInPropertyManager() {
+        aggregateFunctionLang.add("SUMME,SUM");
+        aggregateFunctionLang.add("MITTELWERT,AVG");
+        PropertyForm propertyForm = new PropertyForm(spellings, orders, dateFormats, timeFormats, dateTimeFormats, aggregateFunctionLang, sql);
+        PropertyMapBuilder propertyMapBuilder = new PropertyMapBuilder();
+        propertyMapBuilder.with(aggregateFunctionLang, PropertyOption.AGGREGATEFUNCTIONLANG).build();
+        Assertions.assertEquals(propertyMapBuilder.build(), propertyManager.parseUserOptionsInput(propertyForm));
+
+        Set<?> getSettingsSet = propertyManager.parseUserOptionsInput(propertyForm).get(PropertyOption.AGGREGATEFUNCTIONLANG).getSettings();
+        Assertions.assertEquals(2, getSettingsSet.size());
+        Assertions.assertTrue(getSettingsSet.toString().contains("SUMME"));
+        Assertions.assertTrue(getSettingsSet.toString().contains("SUM"));
+        Assertions.assertTrue(getSettingsSet.toString().contains("AVG"));
+        Assertions.assertTrue(getSettingsSet.toString().contains("MITTELWERT"));
     }
 }
