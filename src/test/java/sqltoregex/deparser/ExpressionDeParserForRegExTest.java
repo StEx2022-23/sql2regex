1e80c5105dde55e707ce6ab97533a48d681effa8
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-17 20:35:26 +0200

    refactored Testing for ExpressionDeParser

--- src/test/java/sqltoregex/deparser/ExpressionDeParserForRegExTest.java
+++ src/test/java/sqltoregex/deparser/ExpressionDeParserForRegExTest.java
@@ -3,12 +3,10 @@ package sqltoregex.deparser;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.expression.Expression;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.statement.select.Select;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.xml.sax.SAXException;
 import sqltoregex.property.SettingsManager;
-import sqltoregex.visitor.SelectVisitorJoinToWhere;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.xpath.XPathExpressionException;
@@ -18,74 +16,85 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ExpressionDeParserForRegExTest {
+import static org.junit.Assert.fail;
+
+class ExpressionDeParserForRegExTest {
 
     private final SettingsManager settingsManager = new SettingsManager();
 
     public ExpressionDeParserForRegExTest() throws XPathExpressionException, ParserConfigurationException, IOException, SAXException {
     }
 
+    private void testDeParsedExpressionVsStringLists(String expressionString, List<String> matchingStrings, List<String> notMatchingStrings){
+        try {
+            Expression expression = (Expression) CCJSqlParserUtil.parseExpression(
+                    expressionString);
+            StringBuilder b = new StringBuilder();
+            ExpressionDeParserForRegEx deParser = new ExpressionDeParserForRegEx(settingsManager);
+            deParser.setBuffer(b);
+            expression.accept(deParser);
+            Pattern pattern = Pattern.compile(b.toString());
+            for (String testString : matchingStrings) {
+                Matcher matcher = pattern.matcher(testString);
+                Assertions.assertTrue(matcher.matches(), "Pattern:" + b + " test String:" + testString);
+            }
+            for (String testString : notMatchingStrings) {
+                Matcher matcher = pattern.matcher(testString);
+                Assertions.assertFalse(matcher.matches(), "Pattern:" + b + " test String:" + testString);
+            }
+        }catch(JSQLParserException e){
+            Assertions.fail("Expression couldn't get parsed");
+        }
+    }
+
     /**
      * Assertions.assertEquals("1\\s+AND\\s+2|2\\s+AND\\s+1", b.toString());
-     * @throws JSQLParserException
      */
     @Test
-    void commutativeBinaryExpression() throws JSQLParserException {
-        Expression expression = (Expression) CCJSqlParserUtil.parseExpression(
-                "1 AND 2");
-        StringBuilder b = new StringBuilder();
-        ExpressionDeParserForRegEx deParser = new ExpressionDeParserForRegEx(settingsManager);
-        deParser.setBuffer(b);
-        expression.accept(deParser);
-        Assertions.assertEquals("\\s*\\s*1\\s*\\s*AND\\s*\\s*2\\s*|\\s*2\\s*\\s*AND\\s*\\s*1\\s*\\s*", b.toString());
+    void commutativeBinaryExpression(){
+        final List<String> matching = new LinkedList<>();
+        matching.add("1 AND 2");
+        matching.add("1   AND   2");
+        final List<String> notMatching = new LinkedList<>();
+        notMatching.add("1 UND 2");
+        notMatching.add("1   &&   2");
+        testDeParsedExpressionVsStringLists("1 AND 2", matching, notMatching);
     }
 
     /**
      * Assertions.assertEquals("3\\s*\\s*2", b.toString());
-     * @throws JSQLParserException
      */
     @Test
-    void binaryExpression() throws JSQLParserException{
-        Expression expression = (Expression) CCJSqlParserUtil.parseExpression(
-                "3 / 2");
-        StringBuilder b = new StringBuilder();
-        ExpressionDeParserForRegEx deParser = new ExpressionDeParserForRegEx(settingsManager);
-        deParser.setBuffer(b);
-        expression.accept(deParser);
-        Assertions.assertEquals("\\s*\\s*3\\s*\\s*/\\s*\\s*2\\s*\\s*", b.toString());
+    void binaryExpression(){
+        final List<String> matching = new LinkedList<>();
+        matching.add("3 / 2");
+        matching.add("3/2");
+        matching.add("3  /  2");
+        final List<String> notMatching = new LinkedList<>();
+        notMatching.add("3:2");
+        testDeParsedExpressionVsStringLists("3 / 2", matching, notMatching);
     }
 
     /**
      * Assertions.assertEquals("a\\s+BETWEEN\\s+1\\s+AND\\s+5", b.toString());
-     * @throws JSQLParserException
      */
     @Test
-    void between() throws JSQLParserException {
-        Expression expression = (Expression) CCJSqlParserUtil.parseExpression(
-                "a BETWEEN 1 AND 5");
-        StringBuilder b = new StringBuilder();
-        ExpressionDeParserForRegEx deParser = new ExpressionDeParserForRegEx(settingsManager);
-        deParser.setBuffer(b);
-        expression.accept(deParser);
-        Assertions.assertEquals("\\s*\\s*a\\s*\\s*BETWEEN\\s*\\s*1\\s*\\s*AND\\s*\\s*5\\s*\\s*", b.toString());
+    void between() {
+        final List<String> matching = new LinkedList<>();
+        matching.add("a BETWEEN 1 AND 5");
+        matching.add(" a BETWEEN 1 AND 5 ");
+        matching.add("a    BETWEEN   1   AND    5");
+        final List<String> notMatching = new LinkedList<>();
+        testDeParsedExpressionVsStringLists("a BETWEEN 1 AND 5", matching, notMatching);
     }
 
     @Test
-    void dateValue() throws JSQLParserException {
-        final List<String> testDates = new LinkedList<>();
-        testDates.add("2022-05-17");
-        testDates.add("2022-5-17");
-        Expression expression = (Expression) CCJSqlParserUtil.parseExpression(
-                "{d'2022-05-17'}");
-        StringBuilder b = new StringBuilder();
-        ExpressionDeParserForRegEx deParser = new ExpressionDeParserForRegEx(settingsManager);
-        deParser.setBuffer(b);
-        expression.accept(deParser);
-        for (String testDate : testDates) {
-            Pattern pattern = Pattern.compile(b.toString());
-            Matcher matcher = pattern.matcher(testDate);
-            Assertions.assertTrue(matcher.matches());
-        }
-
+    void dateValue(){
+        final List<String> matching = new LinkedList<>();
+        matching.add("2022-05-17");
+        matching.add("2022-5-17");
+        final List<String> notMatching = new LinkedList<>();
+        notMatching.add("17.05.2022");
+        testDeParsedExpressionVsStringLists("{d'2022-05-17'}", matching, notMatching);
     }
 }
