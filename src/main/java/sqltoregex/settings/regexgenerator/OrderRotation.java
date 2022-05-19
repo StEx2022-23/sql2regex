3a371b515b2136dc09e8805d38418c0ae15c456d
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-19 13:25:55 +0200

    changes to OrderRotation
    - made constants uppercase
    - formated

--- src/main/java/sqltoregex/settings/regexgenerator/OrderRotation.java
+++ src/main/java/sqltoregex/settings/regexgenerator/OrderRotation.java
@@ -1,10 +1,12 @@
 package sqltoregex.settings.regexgenerator;
 
 import org.springframework.util.Assert;
-import sqltoregex.settings.SettingsOption;
 import sqltoregex.settings.RegExGenerator;
+import sqltoregex.settings.SettingsOption;
 
-import java.util.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * The OrderRotation class allows creating a RegEx expression that takes into all possible order possibilities.
@@ -13,88 +15,96 @@ import java.util.*;
  * SELECT (?:table1\s*,\s*table2|table2\s*,\s*table1)
  */
 public class OrderRotation implements RegExGenerator<List<String>> {
-    private final StringBuilder orderRotationOfValueList = new StringBuilder();
-    private SpellingMistake spellingMistake;
-    private final SettingsOption settingsOption;
+    private final StringBuilder BUFFER = new StringBuilder();
+    private final SettingsOption SETTINGS_OPTION;
     protected boolean isCapturingGroup = false;
+    private SpellingMistake spellingMistake;
 
-    public OrderRotation(SettingsOption settingsOption, SpellingMistake spellingMistake){
+    public OrderRotation(SettingsOption settingsOption, SpellingMistake spellingMistake) {
         Assert.notNull(spellingMistake, "SpellingMistake must not be null");
         Assert.notNull(settingsOption, "SettingsOption must not be null");
         this.spellingMistake = spellingMistake;
-        this.settingsOption = settingsOption;
+        this.SETTINGS_OPTION = settingsOption;
     }
 
-    public OrderRotation(SettingsOption settingsOption){
-        this.settingsOption = settingsOption;
-    }
-
-    /**
-     * helper function for recursive tablename order concatenation
-     * @param amount Integer
-     * @param valueList List<String>
-     */
-    private void orderRotationRek(Integer amount, List<String> valueList){
-        StringBuilder singleValue = new StringBuilder();
-        if (amount == 1) {
-            Iterator<String> iterator = valueList.iterator();
-            while (iterator.hasNext()) {
-                if(spellingMistake != null) {
-                    singleValue.append(spellingMistake.generateRegExFor(iterator.next()));
-                } else{
-                    singleValue.append(iterator.next());
-                }
-                if(iterator.hasNext()){
-                    singleValue.append("\\s*,\\s*");
-                }
-            }
-            singleValue.append("|");
-            orderRotationOfValueList.append(singleValue);
-        } else {
-            orderRotationRek(amount-1, valueList);
-            for(int i = 0; i < amount-1;i++){
-                if (amount % 2 == 0){
-                    String temp;
-                    temp = valueList.get(amount-1);
-                    valueList.set(amount-1, valueList.get(i));
-                    valueList.set(i, temp);
-                } else {
-                    String temp;
-                    temp = valueList.get(amount-1);
-                    valueList.set(amount-1, valueList.get(0));
-                    valueList.set(0, temp);
-                }
-                orderRotationRek(amount-1, valueList);
-            }
-        }
+    public OrderRotation(SettingsOption settingsOption) {
+        this.SETTINGS_OPTION = settingsOption;
     }
 
     /**
      * combine every possible table name order to a non-capturing regex group, optional with alternative writing styles
+     *
      * @param valueList List<String>
      * @return Regex (non-capturing group)
      */
-    public String generateRegExFor(List<String> valueList){
+    public String generateRegExFor(List<String> valueList) {
         Assert.notNull(valueList, "Value list must not be null!");
-        orderRotationOfValueList.append(isCapturingGroup ? '(' : "(?:");
+        BUFFER.append(isCapturingGroup ? '(' : "(?:");
         Integer amountOfElements = valueList.size();
         orderRotationRek(amountOfElements, valueList);
-        orderRotationOfValueList.replace(orderRotationOfValueList.length()-1, orderRotationOfValueList.length(), "");
-        orderRotationOfValueList.append(')');
-        return orderRotationOfValueList.toString();
+        BUFFER.replace(BUFFER.length() - 1, BUFFER.length(), "");
+        BUFFER.append(')');
+        return BUFFER.toString();
     }
 
-    public void setSpellingMistake(SpellingMistake spellingMistake){
-        Assert.notNull(spellingMistake, "Spelling Mistake must not be null");
-        this.spellingMistake = spellingMistake;
+    @Override
+    public SettingsOption getSettingsOption() {
+        return SETTINGS_OPTION;
     }
 
     public SpellingMistake getSpellingMistake() {
         return spellingMistake;
     }
 
+    public void setSpellingMistake(SpellingMistake spellingMistake) {
+        Assert.notNull(spellingMistake, "Spelling Mistake must not be null");
+        this.spellingMistake = spellingMistake;
+    }
+
+    /**
+     * helper function for recursive tablename order concatenation
+     *
+     * @param amount    Integer
+     * @param valueList List<String>
+     */
+    private void orderRotationRek(Integer amount, List<String> valueList) {
+        StringBuilder singleValue = new StringBuilder();
+        if (amount == 1) {
+            Iterator<String> iterator = valueList.iterator();
+            while (iterator.hasNext()) {
+                if (spellingMistake != null) {
+                    singleValue.append(spellingMistake.generateRegExFor(iterator.next()));
+                } else {
+                    singleValue.append(iterator.next());
+                }
+                if (iterator.hasNext()) {
+                    singleValue.append("\\s*,\\s*");
+                }
+            }
+            singleValue.append("|");
+            BUFFER.append(singleValue);
+        } else {
+            orderRotationRek(amount - 1, valueList);
+            for (int i = 0; i < amount - 1; i++) {
+                if (amount % 2 == 0) {
+                    String temp;
+                    temp = valueList.get(amount - 1);
+                    valueList.set(amount - 1, valueList.get(i));
+                    valueList.set(i, temp);
+                } else {
+                    String temp;
+                    temp = valueList.get(amount - 1);
+                    valueList.set(amount - 1, valueList.get(0));
+                    valueList.set(0, temp);
+                }
+                orderRotationRek(amount - 1, valueList);
+            }
+        }
+    }
+
     /**
      * Sets whether there will be an enclosing non capturing group (?: ... ) around the generated regEx.
+     *
      * @param capturingGroup true for capturing group false for non-capturing group
      */
     public void setCapturingGroup(boolean capturingGroup) {
@@ -102,20 +112,16 @@ public class OrderRotation implements RegExGenerator<List<String>> {
     }
 
     @Override
-    public SettingsOption getSettingsOption() {
-        return settingsOption;
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         OrderRotation that = (OrderRotation) o;
-        return Objects.equals(spellingMistake, that.spellingMistake) && settingsOption == that.settingsOption;
+        return Objects.equals(spellingMistake, that.spellingMistake) && SETTINGS_OPTION == that.SETTINGS_OPTION;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(spellingMistake, settingsOption);
+        return Objects.hash(spellingMistake, SETTINGS_OPTION);
     }
+
 }
