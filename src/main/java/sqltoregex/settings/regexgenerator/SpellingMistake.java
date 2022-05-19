aec11b10eaff3112613ff3981e6511f2a91ad298
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-19 13:28:48 +0200

    changes to SpellingsMistake
    - made constants uppercase
    - formated

--- src/main/java/sqltoregex/settings/regexgenerator/SpellingMistake.java
+++ src/main/java/sqltoregex/settings/regexgenerator/SpellingMistake.java
@@ -3,7 +3,7 @@ package sqltoregex.settings.regexgenerator;
 import sqltoregex.settings.RegExGenerator;
 import sqltoregex.settings.SettingsOption;
 
-import java.util.*;
+import java.util.Objects;
 
 /**
  * The OrderRotation class allows creating a RegEx expression that takes into account different spelling variants
@@ -12,34 +12,42 @@ import java.util.*;
  * test ↔ (?:test|est|tst|tet|tes) or (test|est|tst|tet|tes)
  */
 public class SpellingMistake implements RegExGenerator<String> {
-    private final SettingsOption settingsOption;
+    private final SettingsOption SETTINGS_OPTION;
     protected boolean isCapturingGroup = false;
 
-    public SpellingMistake(SettingsOption settingsOption){
-        this.settingsOption = settingsOption;
+    public SpellingMistake(SettingsOption settingsOption) {
+        this.SETTINGS_OPTION = settingsOption;
     }
 
     /**
      * Iterates through each letter of the word, storing all spelling variations assuming a letter is forgotten
+     *
      * @param str String
      * @return creates a regex non-capturing group with all calculated options
      */
-    public String generateRegExFor(String str){
-        if(str.isEmpty()){
+    public String generateRegExFor(String str) {
+        if (str.isEmpty()) {
             throw new IllegalArgumentException("String str should not be empty!");
         }
         StringBuilder alternativeWritingStyles = new StringBuilder();
         alternativeWritingStyles.append(isCapturingGroup ? '(' : "(?:");
         alternativeWritingStyles.append(str).append('|');
-        for(int i = 0; i<str.length(); i++){
-            alternativeWritingStyles.append(str, 0, i).append(str, i+1, str.length()).append("|");
+        for (int i = 0; i < str.length(); i++) {
+            alternativeWritingStyles.append(str, 0, i).append(str, i + 1, str.length()).append("|");
         }
-        alternativeWritingStyles.replace(alternativeWritingStyles.length()-1, alternativeWritingStyles.length(), "");
+        alternativeWritingStyles.replace(alternativeWritingStyles.length() - 1, alternativeWritingStyles.length(), "");
         alternativeWritingStyles.append(')');
         return alternativeWritingStyles.toString();
     }
+
+    @Override
+    public SettingsOption getSettingsOption() {
+        return SETTINGS_OPTION;
+    }
+
     /**
      * Sets whether there will be an enclosing non capturing group (?: ... ) around the generated regEx.
+     *
      * @param capturingGroup true for capturing group false for non-capturing group
      */
     @Override
@@ -48,19 +56,15 @@ public class SpellingMistake implements RegExGenerator<String> {
     }
 
     @Override
-    public SettingsOption getSettingsOption() {
-        return settingsOption;
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof SpellingMistake that)) return false;
-        return settingsOption == that.settingsOption;
+        return SETTINGS_OPTION == that.SETTINGS_OPTION;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(settingsOption);
+        return Objects.hash(SETTINGS_OPTION);
     }
+
 }
