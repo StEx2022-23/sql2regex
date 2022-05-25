14b6ad1908ea3a1b6160f185c9e8fab723d8e09d
binkertpat <58105072+binkertpat@users.noreply.github.com>
2022-05-25 18:19:47 +0200

    add keywordspellingmistake in InsertDeParserForRegEx

--- src/main/java/sqltoregex/deparser/InsertDeParserForRegEx.java
+++ src/main/java/sqltoregex/deparser/InsertDeParserForRegEx.java
@@ -7,10 +7,14 @@ import net.sf.jsqlparser.statement.insert.Insert;
 import net.sf.jsqlparser.statement.select.SubSelect;
 import net.sf.jsqlparser.util.deparser.InsertDeParser;
 import sqltoregex.settings.SettingsManager;
+import sqltoregex.settings.SettingsOption;
+import sqltoregex.settings.regexgenerator.RegExGenerator;
+import sqltoregex.settings.regexgenerator.SpellingMistake;
 
 public class InsertDeParserForRegEx extends InsertDeParser {
     ExpressionDeParserForRegEx expressionDeParserForRegEx;
     SelectDeParserForRegEx selectDeParserForRegEx;
+    RegExGenerator<String> keywordSpellingMistake;
 
     public InsertDeParserForRegEx(SettingsManager settingsManager) {
         this(new ExpressionDeParserForRegEx(settingsManager), new SelectDeParserForRegEx(settingsManager), new StringBuilder(), settingsManager);
@@ -20,8 +24,17 @@ public class InsertDeParserForRegEx extends InsertDeParser {
         super(expressionDeParserForRegEx, selectDeParserForRegEx, buffer);
         this.expressionDeParserForRegEx = expressionDeParserForRegEx;
         this.selectDeParserForRegEx = selectDeParserForRegEx;
+        this.setKeywordSpellingMistake(settingsManager);
     }
 
+    private void setKeywordSpellingMistake(SettingsManager settingsManager){
+        this.keywordSpellingMistake = settingsManager.getSettingBySettingsOption(SettingsOption.KEYWORDSPELLING, SpellingMistake.class).orElse(null);
+    }
+
+    private String useKeywordSpellingMistake(String str){
+        if(null != this.keywordSpellingMistake) return this.keywordSpellingMistake.generateRegExFor(str);
+        else return str;
+    }
 
     @Override
     public void deParse(Insert insert) {
