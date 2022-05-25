f5adc29e5cc85b33c26e1fa745a75770c62ef825
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-05-25 17:47:27 +0200

    removed unnecessary imports

--- src/main/java/sqltoregex/ConverterManagement.java
+++ src/main/java/sqltoregex/ConverterManagement.java
@@ -14,14 +14,10 @@ import net.sf.jsqlparser.util.validation.feature.DatabaseType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
-import org.xml.sax.SAXException;
 import sqltoregex.deparser.ExpressionDeParserForRegEx;
 import sqltoregex.deparser.StatementDeParserForRegEx;
 import sqltoregex.settings.SettingsManager;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPathExpressionException;
-import java.io.IOException;
 import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -34,73 +30,38 @@ import java.util.logging.Logger;
 public class ConverterManagement {
 
     private SettingsManager settingsManager;
+
     /**
      * Validate inserted SQL-statements for oracle, mysql, sqlserver, mariadb
      * return true or false, with a console output about the error messages
+     *
      * @return boolean
      */
 
     @Autowired
-    public ConverterManagement(SettingsManager settingsManager){
+    public ConverterManagement(SettingsManager settingsManager) {
         Assert.notNull(settingsManager, "Settings manager must not be null");
         this.settingsManager = settingsManager;
     }
 
-    public boolean validate(String sqlstatement){
-        List<DatabaseType> supportedDBMS = new ArrayList<>();
-        supportedDBMS.add(DatabaseType.ORACLE);
-        supportedDBMS.add(DatabaseType.MYSQL);
-        supportedDBMS.add(DatabaseType.SQLSERVER);
-        supportedDBMS.add(DatabaseType.MARIADB);
-
-        Validation validation = new Validation(supportedDBMS, sqlstatement);
-        List<ValidationError> validationErrors = validation.validate();
-        if(validationErrors.isEmpty()) { return true;}
-        else{
-            for(ValidationError va : validationErrors){
-                Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-                logger.log(Level.WARNING, String.format("Error while validating the statement: %s",sqlstatement), va);
-            }
-            return false;
-        }
-    }
-
-    private Statement parseStatement(String sqlstatement) throws JSQLParserException {
-        return CCJSqlParserUtil.parse(sqlstatement);
-    }
-
-    private Expression parseExpression(String sqlstatement) throws JSQLParserException {
-        return CCJSqlParserUtil.parseExpression(sqlstatement);
-    }
-
-    /**
-     * compared list elements about unique elements
-     * @param list List<String>
-     * @return boolean
-     */
-    private boolean isDistinctList(List<String> list){
-        if(list.isEmpty()) throw new IllegalArgumentException("List should not be empty. One element is required.");
-        Set<String> set = new HashSet<>(list);
-        return (set.size() == list.size());
-    }
-
-    private String buildOutputRegex(String regex){
+    private String buildOutputRegex(String regex) {
         return "^" + regex + "$";
     }
 
     /**
      * logical OR-concat, if the exprission-visitor detect an alternative statement
+     *
      * @param regexList List<String>
      * @return concatOutputString - String
      */
-    private String buildOutputRegex(List<String> regexList){
+    private String buildOutputRegex(List<String> regexList) {
         StringBuilder outputRegex = new StringBuilder();
         outputRegex.append("^");
 
-        if(regexList.size() == 1 || !isDistinctList(regexList)){
+        if (regexList.size() == 1 || !isDistinctList(regexList)) {
             outputRegex.append(regexList.get(0));
-        } else if(isDistinctList(regexList)){
-            for(String str : regexList){
+        } else if (isDistinctList(regexList)) {
+            for (String str : regexList) {
                 outputRegex.append("(");
                 outputRegex.append(str);
                 outputRegex.append(")|");
@@ -109,18 +70,6 @@ public class ConverterManagement {
         }
         outputRegex.append("$");
         return outputRegex.toString();
-    }
-
-    private String deParseStatement(String sqlStatement, StringBuilder buffer) throws JSQLParserException {
-        Statement statement;
-        statement = this.parseStatement(sqlStatement);
-        StatementDeParserForRegEx defaultStatementDeparser = new StatementDeParserForRegEx(buffer, settingsManager);
-        statement.accept(defaultStatementDeparser);
-        String regExOne = defaultStatementDeparser.getBuffer().toString();
-        ExpressionDeParserForRegEx expressionDeParser = new ExpressionDeParserForRegEx(settingsManager);
-        StatementDeParser joinWhereStatementDeParser = new StatementDeParserForRegEx(expressionDeParser, buffer, settingsManager);
-        String regExTwo = joinWhereStatementDeParser.getBuffer().toString();
-        return this.buildOutputRegex(Arrays.asList(regExOne, regExTwo));
     }
 
     private String deParseExpression(String sqlstatement) throws JSQLParserException {
@@ -133,15 +82,29 @@ public class ConverterManagement {
         return this.buildOutputRegex(expressionDeParser.getBuffer().toString());
     }
 
+    private String deParseStatement(String sqlStatement, StringBuilder buffer) throws JSQLParserException {
+        Statement statement;
+        statement = this.parseStatement(sqlStatement);
+        StatementDeParserForRegEx defaultStatementDeparser = new StatementDeParserForRegEx(buffer, settingsManager);
+        statement.accept(defaultStatementDeparser);
+        String regExOne = defaultStatementDeparser.getBuffer().toString();
+        ExpressionDeParserForRegEx expressionDeParser = new ExpressionDeParserForRegEx(settingsManager);
+        StatementDeParser joinWhereStatementDeParser = new StatementDeParserForRegEx(expressionDeParser, buffer,
+                                                                                     settingsManager);
+        String regExTwo = joinWhereStatementDeParser.getBuffer().toString();
+        return this.buildOutputRegex(Arrays.asList(regExOne, regExTwo));
+    }
+
     /**
      * (de-)parsing the given statement
+     *
      * @param sqlStatement String
      * @return deparsed Statement as RegEx - String
      * @throws JSQLParserException is thrown if parsing goes wrong
      */
     public String deparse(String sqlStatement) throws JSQLParserException {
         StringBuilder buffer = new StringBuilder();
-        if(!this.validate(sqlStatement)){
+        if (!this.validate(sqlStatement)) {
             throw new IllegalArgumentException();
         }
         return this.deParseStatement(sqlStatement, buffer);
@@ -151,15 +114,56 @@ public class ConverterManagement {
         return deparse(sqlStatement, isOnlyExpression, true);
     }
 
-    public String deparse(String sqlStatement, boolean isOnlyExpression, boolean toBeValidated) throws JSQLParserException {
+    public String deparse(String sqlStatement, boolean isOnlyExpression,
+                          boolean toBeValidated) throws JSQLParserException {
         StringBuilder buffer = new StringBuilder();
-        if(isOnlyExpression){
+        if (isOnlyExpression) {
             return this.deParseExpression(sqlStatement);
         } else {
-            if(toBeValidated && !this.validate(sqlStatement)){
+            if (toBeValidated && !this.validate(sqlStatement)) {
                 throw new IllegalArgumentException();
             }
             return this.deParseStatement(sqlStatement, buffer);
+        }
+    }
+
+    /**
+     * compared list elements about unique elements
+     *
+     * @param list List<String>
+     * @return boolean
+     */
+    private boolean isDistinctList(List<String> list) {
+        if (list.isEmpty()) throw new IllegalArgumentException("List should not be empty. One element is required.");
+        Set<String> set = new HashSet<>(list);
+        return (set.size() == list.size());
+    }
+
+    private Expression parseExpression(String sqlstatement) throws JSQLParserException {
+        return CCJSqlParserUtil.parseExpression(sqlstatement);
+    }
+
+    private Statement parseStatement(String sqlstatement) throws JSQLParserException {
+        return CCJSqlParserUtil.parse(sqlstatement);
+    }
+
+    public boolean validate(String sqlstatement) {
+        List<DatabaseType> supportedDBMS = new ArrayList<>();
+        supportedDBMS.add(DatabaseType.ORACLE);
+        supportedDBMS.add(DatabaseType.MYSQL);
+        supportedDBMS.add(DatabaseType.SQLSERVER);
+        supportedDBMS.add(DatabaseType.MARIADB);
+
+        Validation validation = new Validation(supportedDBMS, sqlstatement);
+        List<ValidationError> validationErrors = validation.validate();
+        if (validationErrors.isEmpty()) {
+            return true;
+        } else {
+            for (ValidationError va : validationErrors) {
+                Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+                logger.log(Level.WARNING, String.format("Error while validating the statement: %s", sqlstatement), va);
+            }
+            return false;
         }
     }
 }
