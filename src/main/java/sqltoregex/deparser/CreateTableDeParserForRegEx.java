7cba1eb56fff88a35dbffdc1af93bce41f025744
Alletkla <65451851+Alletkla@users.noreply.github.com>
2022-06-15 17:29:03 +0200

    added Tests for the most grammar options

--- src/main/java/sqltoregex/deparser/CreateTableDeParserForRegEx.java
+++ src/main/java/sqltoregex/deparser/CreateTableDeParserForRegEx.java
@@ -3,6 +3,7 @@ package sqltoregex.deparser;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
+import net.sf.jsqlparser.statement.create.table.ForeignKeyIndex;
 import net.sf.jsqlparser.statement.create.table.Index;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
@@ -32,6 +33,7 @@ public class CreateTableDeParserForRegEx extends CreateTableDeParser {
     private final SpellingMistake tableNameSpellingMistake;
     private final SpellingMistake columnNameSpellingMistake;
     private final StringSynonymGenerator datatypeSynonymGenerator;
+    private final StringSynonymGenerator otherSynonymsGenerator;
 
     /**
      * Short constrcutor for CreateTableDeParserForRegEx.
@@ -54,6 +56,7 @@ public class CreateTableDeParserForRegEx extends CreateTableDeParser {
         this.indexColumnNameOrder = settings.get(OrderRotation.class).get(SettingsOption.INDEXCOLUMNNAMEORDER);
         this.columnNameOrder = settings.get(OrderRotation.class).get(SettingsOption.COLUMNNAMEORDER);
         this.datatypeSynonymGenerator = settings.get(StringSynonymGenerator.class).get(SettingsOption.DATATYPESYNONYMS);
+        this.otherSynonymsGenerator = settings.get(StringSynonymGenerator.class).get(SettingsOption.OTHERSYNONYMS);
     }
 
     private List<String> columnDefinitionsListToStringList(List<ColumnDefinition> columnDefinitions){
@@ -88,16 +91,40 @@ public class CreateTableDeParserForRegEx extends CreateTableDeParser {
                 tmpBuffer.append("|");
             }
 
-            tmpBuffer.append(index.getType());
+            tmpBuffer.append(StringSynonymGenerator.useOrDefault(this.otherSynonymsGenerator, index.getType()));
             tmpBuffer.append(!index.getName().isEmpty() ? REQUIRED_WHITE_SPACE + SpellingMistake.useOrDefault(this.indexColumnNameSpelling, index.getName()) : "");
             tmpBuffer.append(REQUIRED_WHITE_SPACE);
             tmpBuffer.append(concatIndexColumns(index));
             if (index.getType() != null && (index.getType().equals("PRIMARY KEY") || index.getType().equals("FOREIGN KEY"))){
                 tmpBuffer.append(")");
             }
+            if (index instanceof ForeignKeyIndex foreignKeyIndex){
+                tmpBuffer.append(OPTIONAL_WHITE_SPACE + "REFERENCES" + REQUIRED_WHITE_SPACE)
+                        .append(foreignKeyIndex.getTable().toString())
+                        .append(OPTIONAL_WHITE_SPACE)
+                        .append("\\(")
+                        .append(OPTIONAL_WHITE_SPACE)
+                        .append(OrderRotation.useOrDefault(this.indexColumnNameOrder, foreignKeyIndex.getReferencedColumnNames()))
+                        .append(OPTIONAL_WHITE_SPACE)
+                        .append("\\)");
+                //Generate the index String with the toString method since there is private access necessary.
+                //Built the public part and remove it to have maximum variety in the string
+                String s =  indexToString(index);
+                tmpBuffer.append(index.toString()
+                                         .replace(s, "")
+                                         .replace(" ", REQUIRED_WHITE_SPACE)); //Spaces between referential Actions
+            }
             stringList.add(tmpBuffer.toString());
         }
         return stringList;
+    }
+    
+    private String indexToString(Index index){
+        String idxSpecText = PlainSelect.getStringList(index.getIndexSpec(), false, false);
+        return (index.getName() != null ? "CONSTRAINT " + index.getName() + " " : "")
+                + index.getType() + " " + PlainSelect.getStringList(index.getColumnsNames(), true, true) + (!"".
+                equals(idxSpecText) ? " " + idxSpecText : "") + " REFERENCES "  + ((ForeignKeyIndex) index).getTable() +
+                PlainSelect.getStringList(((ForeignKeyIndex) index).getReferencedColumnNames(), true, true);
     }
 
     private String getProcessedTableOptions(CreateTable createTable){
@@ -233,11 +260,19 @@ public class CreateTableDeParserForRegEx extends CreateTableDeParser {
         List<String> synsWithSpellingMistake = new LinkedList<>();
 
         for (String keywordSyn : StringSynonymGenerator.generateAsListOrDefault(this.datatypeSynonymGenerator, columnDefinition.getColDataType().toString())){
+            if (keywordSyn.matches(".*\\([\\w,\\s]+\\)")){
+                keywordSyn = keywordSyn.replace("(", "\\(");
+                keywordSyn = keywordSyn.replace(")", "\\)");
+            }
             synsWithSpellingMistake.add(SpellingMistake.useOrDefault(this.keywordSpellingMistake, keywordSyn));
         }
         buffer.append(RegExGenerator.joinListToRegEx(this.datatypeSynonymGenerator, synsWithSpellingMistake));
         if (columnDefinition.getColumnSpecs() != null) {
             for (String s : columnDefinition.getColumnSpecs()) {
+                if (s.contains("(") && s.contains(")")){
+                   s = s.replace("(", "\\(");
+                   s = s.replace(")", "\\)");
+                }
                 buffer.append(REQUIRED_WHITE_SPACE);
                 buffer.append(SpellingMistake.useOrDefault(this.keywordSpellingMistake, s));
             }
